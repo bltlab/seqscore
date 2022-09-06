6fa86620bd7651646babc05ef425af02b6b2406e
Chester Palen-Michel <cpalenmichel@brandeis.edu>
2022-09-06 17:54:35 -0400

    Make adjudication able to run ~100 times faster and some refactoring

--- seqscore/adjudication.py
+++ seqscore/adjudication.py
@@ -1,6 +1,6 @@
 import os
 import re
-from collections import Counter, defaultdict
+from collections import Counter, defaultdict, namedtuple
 from math import log
 from typing import (
     AbstractSet,
@@ -23,6 +23,8 @@ from seqscore.model import LabeledSequence, Mention, SequenceProvenance, Span
 ENTITY = "entity"
 NON_ENTITY = "non_entity"
 
+Unit = namedtuple("Unit", ("starting_line", "span"))
+
 
 @attrs(frozen=True, slots=True, auto_attribs=True)
 class AnnotatorMention:
@@ -32,6 +34,10 @@ class AnnotatorMention:
 
 @attrs(frozen=True, slots=True)
 class AnnotatorLabeledSequence:
+    """
+    Label sequence with labels from different annotators.
+    """
+
     tokens: Tuple[str, ...] = attrib()
     mentions_by_span: Dict[Span, List[AnnotatorMention]] = attrib(factory=dict)
     labels_by_annotator: Dict[int, Tuple[str, ...]] = attrib(factory=dict)
@@ -44,25 +50,6 @@ class AnnotatorLabeledSequence:
 
     def span_tokens(self, span: Span) -> Tuple[str, ...]:
         return self.tokens[span.start : span.end]
-
-
-def sus_sentence_ending(labelsequence: LabeledSequence) -> bool:
-    if len(labelsequence) < 4:
-        return True
-    elif re.fullmatch(r"[A-Z]\d+", labelsequence.tokens[-2]):
-        # Don't mark N380 as a bad sentence candidate since it's currency
-        return False
-    # XXX: Are there other punctuations we want to account for? Ge'ez etc?
-    elif (
-        labelsequence.tokens[-2][0].isupper()
-        and labelsequence.tokens[-1] == "."
-        and len(labelsequence.tokens[-2]) < 5
-    ):
-        # check if second to last token has upper case first character and is followed by a . and is short
-        # Checking for Dr. /Dkt .
-        return True
-    else:
-        return False
 
 
 @attrs
@@ -124,10 +111,6 @@ class AdjudicationSequence:
             ]
             comment = self.comment_labels[i] if self.comment_labels[i] else ""
             if i in conflicting_indices:
-                # TODO Leave more useful comment about what conflict is
-                # TODO: use entropy stuff to leave hints
-                # TODO: Add comment labels to the AdjudicationSequence,
-                #  so can zip them in when they exist
                 if prev_label != "ADJ":
                     conflict_prefix = f"conflict-todo:{comment}"
                 else:
@@ -182,7 +165,8 @@ class MentionCounts:
             )
             for mention_type in self.mention_types(mention)
         ]
-        return -1 * sum(p * log(p) for p in ps if p != 0)
+        # Floats in sum are fine, yet mypy complains
+        return -1 * sum(p * log(p) for p in ps if p != 0)  # type: ignore
 
     def most_common_types(
         self, mention: Tuple[str, ...], n: Optional[int] = None
@@ -243,19 +227,44 @@ class AdjudicationResult:
     disagreements: DisagreementResult = attrib()
 
 
+def sus_sentence_ending(labelsequence: LabeledSequence) -> bool:
+    """
+    Checks for suspicious sentence endings.
+    """
+    if len(labelsequence) < 4:
+        return True
+    elif re.fullmatch(r"[A-Z]\d+", labelsequence.tokens[-2]):
+        # Don't mark N380 as a bad sentence candidate since it's currency
+        return False
+    # XXX: Are there other punctuations we want to account for? Ge'ez etc?
+    elif (
+        labelsequence.tokens[-2][0].isupper()
+        and labelsequence.tokens[-1] == "."
+        and len(labelsequence.tokens[-2]) < 5
+    ):
+        # Check if second to last token has upper case first character
+        # and is followed by a . and is short
+        # Checking for Dr. /Dkt .
+        return True
+    else:
+        return False
+
+
 def new_labels_from_mentions(
     mentions: Sequence[Mention], seq_len: int
 ) -> Tuple[str, ...]:
-    # TODO: Handle other encoding schemes probably, ugh.
     seq = ["O" for _ in range(seq_len)]
     for mention in mentions:
+        # XXX: Currently only considers BIO mention encoding
         seq[mention.span.start] = f"B-{mention.type}"
         for i in range(mention.span.start + 1, mention.span.end):
             seq[i] = f"I-{mention.type}"
     return tuple(seq)
 
 
-def ngrams(tokens: Sequence[str], n: int = 1) -> Generator[Tuple[str, ...], None, None]:
+def ngrams(
+        tokens: Sequence[Union[str, Tuple[str, str], Tuple[str, int]]], n: int = 1
+) -> Generator[Tuple[Union[str, Tuple[str, str], Tuple[str, int]], ...], None, None]:
     """
     Gets ngrams for a sequence of tokens with specified n
     """
@@ -266,7 +275,12 @@ def ngrams(tokens: Sequence[str], n: int = 1) -> Generator[Tuple[str, ...], None
 def yield_all_ngrams(
     tokens: Sequence[Union[str, Tuple[str, str], Tuple[str, int]]],
     max_n: Optional[int] = None,
-) -> Generator[Tuple[Union[str, Tuple[str, str], Tuple[str, int]]], None, None]:
+) -> Generator[Tuple[Union[str, Tuple[str, str], Tuple[str, int]], ...], None, None]:
+    """
+    Generates all n-grams for a sequence.
+    `max_n` caps the length of ngrams.
+    ie. `max_n` of 3 gives unigrams, bigrams, and trigrams.
+    """
     if max_n is None:
         max_n = len(tokens)
     max_n = min([len(tokens), max_n])
@@ -278,7 +292,7 @@ def yield_all_ngrams(
 
 def count_mention_spans(
     mention_type_counts: MentionCounts, docs: List[List[LabeledSequence]]
-) -> Dict[Tuple[str], int]:
+) -> Mapping[Tuple[str, ...], int]:
     counts = Counter(
         {mention_tokens: 0 for mention_tokens in mention_type_counts.mention_tokens()}
     )
@@ -287,18 +301,18 @@ def count_mention_spans(
         for labelsequence in doc:
             for ngram in yield_all_ngrams(labelsequence.tokens, max_ent_len):
                 if ngram in counts:
-                    counts[ngram] += 1
+                    # ngram is Tuple[str] in this case
+                    counts[ngram] += 1  # type: ignore
     return counts
 
 
 def has_outside(token_label_span: Sequence[Tuple[str, str]]) -> bool:
     """Given list of token, label tuples, do any have outside"""
-    # TODO: Should this be made to handle other label schemes?
     return any([t[1] == "O" for t in token_label_span])
 
 
 def fleiss_kappa(
-    units_to_labels: Mapping[Tuple[int, Span], Counter],
+    units_to_labels: Mapping[Unit, Counter],
     num_annotators: int,
     labels: AbstractSet[str],
 ) -> float:
@@ -372,9 +386,9 @@ def add_comment(comment_labels: List[Optional[str]], span: Span, comment: str) -
 def token_level_type_clash(
     labels_by_annotator: Dict[int, Tuple[str, ...]], span: Span
 ) -> bool:
-
+    # XXX: BIO specific logic
     span_labels = [
-        [label.lstrip("B-").lstrip("I-") for label in labels[span.start : span.end]]
+        [label.lstrip("B-").lstrip("I-") for label in labels[span.start: span.end]]
         for labels in labels_by_annotator.values()
     ]
     for labels in zip(*span_labels):
@@ -399,7 +413,8 @@ def check_names_to_flag(
             and mentions_to_flag_by_name[tokens] == ENTITY
         ):
             # This uses the token indices, so +1 to the end token index
-            spans.append(Span(int(token_seq[0][1]), token_seq[-1][1] + 1))
+            # Mypy is getting confused with the tuples from ngram generator
+            spans.append(Span(int(token_seq[0][1]), token_seq[-1][1] + 1))  # type: ignore
     return spans
 
 
@@ -414,80 +429,95 @@ def check_disagreements(
     disagreements = 0
     disaggrements_by_type = 0
     disagreements_by_span = 0
-    units_to_labels = defaultdict(Counter)
-    units_to_mentions = defaultdict(list)
+    units_to_labels: Mapping[Unit, Counter] = defaultdict(Counter)
+    units_to_mentions: DefaultDict[Unit, List[AnnotatorMention]] = defaultdict(list)
     labels = set("O")
     majority_voted_mentions = defaultdict(list)
-    conflict_mentions = defaultdict(list)
+    conflict_mentions: DefaultDict[int, List[AnnotatorMention]] = defaultdict(list)
     label_sequences_by_line = {}
+
     for label_sequence in annotator_label_sequences:
         label_sequences_by_line[label_sequence.provenance.starting_line] = label_sequence
         for span in label_sequence.mentions_by_span:
-            unit = (label_sequence.provenance.starting_line, span)
+            unit = Unit(label_sequence.provenance.starting_line, span)
             for annotator_mention in label_sequence.mentions_by_span[span]:
                 labels.add(annotator_mention.mention.type)
                 units_to_labels[unit][annotator_mention.mention.type] += 1
                 units_to_mentions[unit].append(annotator_mention)
-    comment_labels_by_line = {
+
+    comment_labels_by_line: Dict[int, List[Optional[str]]] = {
         label_sequence.provenance.starting_line: [
             None for _ in range(len(label_sequence))
         ]
         for label_sequence in annotator_label_sequences
     }
+
     for unit in units_to_labels:
         if sum(units_to_labels[unit].values()) > num_annotators / 2 and no_overlap(
-            units_to_mentions[unit], label_sequences_by_line[unit[0]]
+            units_to_mentions[unit], label_sequences_by_line[unit.starting_line]
         ):
             # Add majority type if more than half annotators choose the span
-            majority_voted_mentions[unit[0]].append(
-                Mention(unit[1], units_to_labels[unit].most_common(1)[0][0])
+            majority_voted_mentions[unit.starting_line].append(
+                Mention(unit.span, units_to_labels[unit].most_common(1)[0][0])
             )
         else:
-            conflict_mentions[unit[0]].extend(
+            conflict_mentions[unit.starting_line].extend(
                 mention for mention in units_to_mentions[unit]
             )
         if num_annotators - sum(units_to_labels[unit].values()) == 0:
             if different_types(units_to_labels[unit]):
                 disaggrements_by_type += 1
                 disagreements += 1
-                add_comment(comment_labels_by_line[unit[0]], unit[1], "type")
+                add_comment(comment_labels_by_line[unit.starting_line], unit.span, "type")
         else:
             disagreements += 1
             disagreements_by_span += 1
             if token_level_type_clash(
-                label_sequences_by_line[unit[0]].labels_by_annotator, unit[1]
+                label_sequences_by_line[unit.starting_line].labels_by_annotator, unit.span
             ):
-                add_comment(comment_labels_by_line[unit[0]], unit[1], "span-type")
+                add_comment(
+                    comment_labels_by_line[unit.starting_line], unit.span, "span-type"
+                )
             else:
-                add_comment(comment_labels_by_line[unit[0]], unit[1], "span")
+                add_comment(comment_labels_by_line[unit.starting_line], unit.span, "span")
         # Type entropy check
         for mention in units_to_mentions[unit]:
             if (
-                label_sequences_by_line[unit[0]].span_tokens(mention.mention.span)
+                label_sequences_by_line[unit.starting_line].span_tokens(
+                    mention.mention.span
+                )
                 in mentions_to_flag_by_type
             ):
                 if (
                     units_to_labels[unit].most_common(1)[0][0]
                     != mentions_to_flag_by_type[
-                        label_sequences_by_line[unit[0]].span_tokens(mention.mention.span)
+                        label_sequences_by_line[unit.starting_line].span_tokens(
+                            mention.mention.span
+                        )
                     ]
                 ):
-                    add_comment(comment_labels_by_line[unit[0]], unit[1], "check-type")
+                    add_comment(
+                        comment_labels_by_line[unit.starting_line],
+                        unit.span,
+                        "check-type",
+                    )
 
     for unit in units_to_labels:
         units_to_labels[unit]["O"] += num_annotators - sum(units_to_labels[unit].values())
         for span in check_names_to_flag(
-            mentions_to_flag_by_name, label_sequences_by_line[unit[0]]
+            mentions_to_flag_by_name, label_sequences_by_line[unit.starting_line]
         ):
-            # TODO: restrict to only likely names that are adjudicated to O?
-            # Condition to restrict this to only O labels!
+            # Restrict to only O labels for check name, no need to check name if tagged as name
             majority_labels = new_labels_from_mentions(
-                majority_voted_mentions[unit[0]], len(label_sequences_by_line[unit[0]])
+                majority_voted_mentions[unit.starting_line],
+                len(label_sequences_by_line[unit.starting_line]),
             )
             if all(
                 maj_label == "O" for maj_label in majority_labels[span.start : span.end]
             ):
-                add_comment(comment_labels_by_line[unit[0]], span, "check-name")
+                add_comment(
+                    comment_labels_by_line[unit.starting_line], span, "check-name"
+                )
     f_kappa = fleiss_kappa(units_to_labels, num_annotators, labels)
     new_sentences = tuple(
         [
@@ -545,8 +575,13 @@ def write_entropy_info(entropy_out_dir: str, adjudication_results: AdjudicationR
             entropy,
         ) in adjudication_results.overall_entity_counts.highest_entropy_mentions():
             if entropy > 0.0:
+                most_common_type = (
+                    adjudication_results.overall_entity_counts.most_common_types(
+                        mention, n=1
+                    )[0][0]
+                )
                 print(
-                    f"{' '.join(mention)}\t{entropy}\t{adjudication_results.overall_entity_counts.most_common_types(mention, n=1)[0][0]}",
+                    f"{' '.join(mention)}\t{entropy}\t{most_common_type}",
                     file=ent_entropy_file,
                 )
     with open(type_entropy_filepath, "w", encoding="utf8") as type_entropy_file:
@@ -555,16 +590,22 @@ def write_entropy_info(entropy_out_dir: str, adjudication_results: AdjudicationR
             entropy,
         ) in adjudication_results.overall_type_counts.highest_entropy_mentions():
             if entropy > 0.0:
+                most_common_type = (
+                    adjudication_results.overall_type_counts.most_common_types(
+                        mention, n=1
+                    )[0][0]
+                )
                 print(
-                    f"{' '.join(mention)}\t{entropy}\t{adjudication_results.overall_type_counts.most_common_types(mention, n=1)[0][0]}",
+                    f"{' '.join(mention)}\t{entropy}\t{most_common_type}",
                     file=type_entropy_file,
                 )
 
 
 def adjudicate_documents(
-    docs_by_annotator: Dict[int, List[List[LabeledSequence]]]
+    docs_by_annotator: Dict[int, List[List[LabeledSequence]]],
+    max_ngram_length_consideration: int = 5,
 ) -> AdjudicationResult:
-
+    # Various counting mechanisms
     entity_entropy_by_annotator = {
         annotator: MentionCounts() for annotator in docs_by_annotator
     }
@@ -577,8 +618,9 @@ def adjudicate_documents(
     single_annotator_label_sequence = [
         label_seq for doc in docs_by_annotator[0] for label_seq in doc
     ]
-    seq_id_to_span_to_mentions = defaultdict(lambda: defaultdict(list))
-    labels_by_annotator_by_seq = defaultdict(dict)
+    seq_id_to_span_to_mentions: DefaultDict[int, DefaultDict[Span, List[AnnotatorMention]]] = defaultdict(lambda: defaultdict(list))
+    labels_by_annotator_by_seq: DefaultDict[int, Dict[int, Tuple[str, ...]]] = defaultdict(dict)
+    # Count everything and setup data structures for use later
     for annotator in docs_by_annotator:
         for doc in docs_by_annotator[annotator]:
             for labeled_sequence in doc:
@@ -587,29 +629,27 @@ def adjudicate_documents(
                     annotator
                 ] = labeled_sequence.labels
                 for token_label_span in yield_all_ngrams(
-                    labeled_sequence.tokens_with_labels()
+                    labeled_sequence.tokens_with_labels(),
+                    max_n=max_ngram_length_consideration,
                 ):
                     tokens = tuple(t[0] for t in token_label_span)
-                    if has_outside(token_label_span):
+                    # token_label_span is Tuple[Tuple[str, str], ...]
+                    if has_outside(token_label_span):  # type: ignore
                         entity_entropy_by_annotator[annotator].put(tokens, NON_ENTITY)
                         overall_entity_entropy.put(tokens, NON_ENTITY)
                     else:
                         entity_entropy_by_annotator[annotator].put(tokens, ENTITY)
                         overall_entity_entropy.put(tokens, ENTITY)
 
+                # Create annotator mentions where annotator info is on mention
                 for mention in labeled_sequence.mentions:
-
-                    # Count mentions
                     mention_tokens = labeled_sequence.mention_tokens(mention)
                     type_entropy_by_annotator[annotator].put(mention_tokens, mention.type)
                     overall_type_entropy.put(mention_tokens, mention.type)
-
-                    # annotator_mentions_by_span[mention.span].append(
-                    #     AnnotatorMention(mention, annotator)
-                    # )
                     seq_id_to_span_to_mentions[labeled_sequence.provenance.starting_line][
                         mention.span
                     ].append(AnnotatorMention(mention, annotator))
+
     annotator_label_sequences = [
         AnnotatorLabeledSequence(
             labeled_sequence.tokens,
@@ -619,6 +659,7 @@ def adjudicate_documents(
         )
         for labeled_sequence in single_annotator_label_sequence
     ]
+
     disagreements = check_disagreements(
         annotator_label_sequences,
         num_annotators=len(docs_by_annotator),
