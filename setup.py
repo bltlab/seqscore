ea29f4d2b621f7c92d4a3d88cc977fb78788f8fd
Constantine Lignos <lignos@brandeis.edu>
2022-11-04 15:46:38 -0400

    Add classifier for Python 3.11 to setup

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ def setup_package() -> None:
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
             "Topic :: Scientific/Engineering :: Artificial Intelligence",
         ],
         url="https://github.com/bltlab/seqscore",
