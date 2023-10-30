b896c1fb5eb7d8ea62b2bc7b84b83edd2a2e2270
Constantine Lignos <lignos@brandeis.edu>
2023-10-30 09:42:38 -0400

    Improve bash usage in pre_commit.sh

--- tests/pre_commit.sh
+++ tests/pre_commit.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-files='seqscore/ tests/ *.py'
-ruff format $files
-ruff $files
-mypy $files
+files=(seqscore/ tests/ *.py)
+ruff format "${files[@]}"
+ruff "${files[@]}"
+mypy "${files[@]}"
 pytest --cov-report term-missing --cov=seqscore tests/
