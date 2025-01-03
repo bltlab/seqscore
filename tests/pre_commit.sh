2f40635ad0b5592a4c00d0ef4b3bdb2748ce8878
Constantine Lignos <lignos@brandeis.edu>
2025-01-03 09:49:02 -0500

    Explain extra usage of ruff check in pre_commit

--- tests/pre_commit.sh
+++ tests/pre_commit.sh
@@ -5,6 +5,6 @@ files=(seqscore/ tests/ *.py)
 ruff check --fix "${files[@]}"
 ruff check --select I --fix "${files[@]}"  # Organize imports
 ruff format "${files[@]}"
-ruff check "${files[@]}"
+ruff check "${files[@]}"  # Redundant but ensures CI will pass
 mypy "${files[@]}"
 pytest --cov-report term-missing --cov=seqscore tests/
